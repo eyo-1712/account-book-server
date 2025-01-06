56d5661e231498b063b36c455787a044565a7b94
knsnyo <isc963@naver.com>
2025-01-06 21:48:40 +0900

    style: req -> request, res -> response

--- src/controllers/summary/index.ts
+++ src/controllers/summary/index.ts
@@ -8,8 +8,8 @@ import { TController } from '../type'
 import { schema } from './schema'
 
 export const summaryControlller: TController = {
-  create: async (req: Request, res: Response, next: NextFunction) => {
-    const { body } = req
+  create: async (request: Request, response: Response, next: NextFunction) => {
+    const { body } = request
 
     if (!schema.safeParse(body)) {
       return next(new AppError(400, 'invalid schema'))
@@ -17,22 +17,20 @@ export const summaryControlller: TController = {
 
     const summary: Summary = await prisma.summary.create(body)
 
-    res.status(201).json({
+    response.status(201).json({
       statusCode: 201,
       success: true,
       data: summary,
     } as ISuccessResponse<Summary>)
   },
-  fetchMonth: (req: Request, res: Response) => {
+  fetchMonth: (request: Request, response: Response) => {
     //
   },
-  fetchDate: (req: Request, res: Response) => {
+  fetchDate: (request: Request, response: Response) => {
     //
   },
-  modify: (req: Request, res: Response) => {
-    //
-  },
-  remove: (req: Request, res: Response) => {
+  modify: (request: Request, response: Response) => {},
+  remove: (request: Request, response: Response) => {
     //
   },
 }
