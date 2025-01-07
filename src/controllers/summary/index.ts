0c3496bf94599378cf59afad012c5081ad818dbb
knsnyo <isc963@naver.com>
2025-01-07 21:49:43 +0900

    feat: read summary data

--- src/controllers/summary/index.ts
+++ src/controllers/summary/index.ts
@@ -23,19 +23,70 @@ export const summaryControlller: TController = {
       data: summary,
     } as ISuccessResponse<Summary>)
   },
-  fetchMonth: (request: Request, response: Response) => {},
-  fetchDate: async (
+  fetchMonth: async (
     request: Request,
     response: Response,
     next: NextFunction,
-  ) => {},
+  ) => {
+    const { query } = request
+
+    const date = query?.date
+
+    if (!date) {
+      return next(new AppError(400, 'Invalid Date'))
+    }
+    if (typeof date !== 'string') {
+      return next(new AppError(400, 'Invalid Date'))
+    }
+    if (!/^\d{4}-(0[1-9]|1[0-2])$/.test(date)) {
+      return next(new AppError(400, 'Invalid Date'))
+    }
+
+    const [year, month] = date.split('-')
+
+    const summaries: Summary[] = await prisma.summary.findMany({
+      where: {
+        createdAt: {
+          gte: new Date(`${year}-${month}-01`),
+          lt: new Date(
+            `${year}-${String(Number(month) + 1).padStart(2, '0')}-01`,
+          ),
+        },
+      },
+    })
+
+    response.status(200).json({
+      statusCode: 200,
+      success: true,
+      data: summaries,
+    } as ISuccessResponse<Summary[]>)
+  },
+  fetchId: async (request: Request, response: Response, next: NextFunction) => {
+    const {
+      params: { id },
+    } = request
+
+    if (!id) return next(new AppError(400, 'Invalid Id'))
+
+    const summary: Summary | null = await prisma.summary.findFirst({
+      where: { id: parseInt(id) },
+    })
+
+    if (!summary) return next(new AppError(404, 'Not Found'))
+
+    response.status(200).json({
+      statusCode: 200,
+      success: true,
+      data: summary as Summary,
+    } as ISuccessResponse<Summary>)
+  },
   modify: async (request: Request, response: Response, next: NextFunction) => {
     const { body, params } = request
 
-    if (!params?.id) return next(new AppError(400, 'invalid schema'))
+    if (!params?.id) return next(new AppError(400, 'Invalid schema'))
 
     if (!schema.safeParse(body)) {
-      return next(new AppError(400, 'invalid schema'))
+      return next(new AppError(400, 'Invalid schema'))
     }
 
     const summary: Summary = await prisma.summary.update({
